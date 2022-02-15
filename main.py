a783793921b37f1b1a7e7539e503637d2e55829d
YTCole <99440010+Samc20094@users.noreply.github.com>
2022-02-15 01:44:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+class NumberGuessingGame:
+
+  def __init__(self):
+    #defining the range
+      self.LOWER = 1
+      self.HIGHER = 10
+
+#generatiing random number
+def get_random_number(self):
+  return random.randit(self.LOWER, self.HIGHER)
+
+#game starter 
